4a6bd5d804972f08e99222493052f2bf9033c9ff
Miltios <45502663+miltios@users.noreply.github.com>
2019-07-12 08:48:21 -0400

    Added mouseover explanation of alacrity tiers

--- src/main/webapp/scripts/HelpController.js
+++ src/main/webapp/scripts/HelpController.js
@@ -7,11 +7,14 @@ let HelpController = (function()
     HelpController.prototype.init = function()
     {
         this.content = document.getElementById('helpDivContent');
-    }
-    HelpController.prototype.display = function(messageType)
+        this.lastMsgType = 'welcome';
+
+        this.addHoverTip(document.getElementById('calcStatAlacrity'), 'alacTiers');
+    };
+    HelpController.prototype.display = function(msgType, isTransient)
     {
         let msg = '';
-        switch(messageType)
+        switch(msgType)
         {
             case 'welcome':
             {
@@ -31,10 +34,43 @@ let HelpController = (function()
                     'If the item is moddable, you can also click the individual mods to swap them out.';
                 break;
             }
+            case 'alacTiers':
+            {
+                msg = 'Each alacrity tier reduces the length of your global cooldown (GCD) by 0.1 seconds.  The tiers are as follows:<br/>' +
+                    'Tier 0 (1.5s): 0.00% to 7.142%<br/>' +
+                    'Tier 1 (1.4s): 7.143% to 15.384%<br/>' +
+                    'Tier 2 (1.3s): 15.385% and higher';
+                break;
+            }
         }
 
         this.content.innerHTML = msg;
-    }
+        if(!isTransient)
+        {
+            this.lastMsgType = msgType;
+        }
+    };
+    HelpController.prototype.addHoverTip = function(el, msgType)
+    {
+        if(el === null)
+        {
+            console.error('Cannot add hover tip to null element!');
+            return;
+        }
+        let me = this;
+        el.addEventListener('mouseenter', function(event)
+        {
+            me.display(msgType, true);
+        });
+        el.addEventListener('mouseout', function(event)
+        {
+            me.revert();
+        });
+    };
+    HelpController.prototype.revert = function()
+    {
+        this.display(this.lastMsgType);
+    };
     return new HelpController();
 })();
 declareReady('HelpController.js', function(){HelpController.init()});
