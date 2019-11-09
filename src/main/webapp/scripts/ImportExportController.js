db65d4d99777c9087edc038e12a2f5736c54d92b
Miltios <45502663+miltios@users.noreply.github.com>
2019-11-09 12:22:56 -0500

    Import functionality for settings

--- src/main/webapp/scripts/ImportExportController.js
+++ src/main/webapp/scripts/ImportExportController.js
@@ -145,6 +145,104 @@ let ImportExportController = (function()
 
         return itemStr;
     };
+    ImportExportController.prototype.importFromString = function(str)
+    {
+        //TODO
+        //return "true" or an error message
+        if(!str)
+        {
+            return 'EMPTY_STRING';
+        }
+        str = atob(str);
+        str.replace(/&com;/g, ',');
+        str.replace(/&dot;/g, '.');
+        str.replace(/&pipe;/g, '|');
+        let codes = str.split('|');
+        if(codes.length !== 3)
+        {
+            return 'INVALID_SECTIONS';
+        }
+        try
+        {
+            this.importSettingsFromString(codes[0]);
+        }
+        catch(e)
+        {
+            console.error(e);
+            return 'INVALID_SETTINGS';
+        }
+        try
+        {
+            this.importGearFromString(codes[0]);
+        }
+        catch(e)
+        {
+            console.error(e);
+            return 'INVALID_GEAR';
+        }
+        try
+        {
+            this.importAugmentsFromString(codes[0]);
+        }
+        catch(e)
+        {
+            console.error(e);
+            return 'INVALID_AUGMENTS';
+        }
+        return true;
+    };
+    ImportExportController.prototype.importSettingsFromString = function(str)
+    {
+        let codes = str.split('.');
+
+        //faction
+        let factionEl = document.getElementsByClassName('faction-select-button-' + codes[0])[0];
+        factionEl.click();
+
+        //class
+        document.getElementById('classDropdown').value = codes[1];
+        Settings.updateSpecDropdown();
+
+        //spec
+        document.getElementById('specDropdown').value = codes[2];
+
+        //datacrons
+        codes[3].split(',').map((datacron) => {
+            document.getElementById('datacronsCheckbox' + datacron).checked = true;
+        });
+        Settings.updateDatacrons(null);
+
+        //class buffs
+        codes[4].split(',').map((buff) => {
+            document.getElementById('classBuffsCheckbox' + buff).checked = true;
+        });
+        Settings.updateClassBuffs(null);
+
+        //companion buffs
+        codes[5].split(',').map((buff) => {
+            document.getElementById('companionBuffsCheckbox' + buff).checked = true;
+        });
+        Settings.updateCompanionBuffs(null);
+
+        //stim
+        let stim = codes[6];
+        if(stim !== 'none')
+        {
+            document.querySelector(`[data-stim-type="${stim}"]`).click();
+        }
+        else
+        {
+            document.getElementsByClassName('button-stim-selected')[0].click();
+        }
+    };
+    ImportExportController.prototype.importGearFromString = function(str)
+    {
+        //TODO
+    };
+    ImportExportController.prototype.importAugmentsFromString = function(str)
+    {
+        //TODO
+    };
     return new ImportExportController();
 })();
 declareReady('ImportExportController.js',  function(){ImportExportController.init()});
